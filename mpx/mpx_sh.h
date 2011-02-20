d5be331004fea52389afe3c2b6cc82288d67374c
Paul Prince <paul@littlebluetech.com>
2011-02-19 22:43:02 -0500

    First code to be comitted!

---
+++ mpx/mpx_sh.h
@@ -0,0 +1,6 @@
+#ifndef MPX_SH_H_GUARD
+#define MPX_SH_H_GUARD
+
+void mpx_shell(void);
+
+#endif
---
+++ mpx/mpx_util.h
@@ -0,0 +1,6 @@
+#ifndef MPX_UTIL_H_GUARD
+#define MPX_UTIL_H_GUARD
+
+#define MAX_LINE	255
+
+#endif
