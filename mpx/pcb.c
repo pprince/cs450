23edaf211574362d7c5deea550732547d9e80204
Paul Prince <paul@littlebluetech.com>
2011-03-17 10:20:08 -0400

    Extremely minor tab-alignment changes.

--- mpx/pcb.c
+++ mpx/pcb.c
@@ -11,11 +11,12 @@
 #include "mpx_util.h"
 
 
-static pcb_queue_t	queue_ready;
-static pcb_queue_t	queue_blocked;
-static pcb_queue_t	queue_susp_ready;
-static pcb_queue_t	queue_susp_blocked;
-pcb_queue_t *queues[4];
+static	pcb_queue_t	queue_ready;
+static	pcb_queue_t	queue_blocked;
+static	pcb_queue_t	queue_susp_ready;
+static	pcb_queue_t	queue_susp_blocked;
+
+	pcb_queue_t	*queues[4];
 
 
 /*! Must be called before using any other PCB or queue functions. */
