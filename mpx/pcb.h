652e20d9140e0ee94d3fb7b38f50e86d58d0c9f5
Paul Prince <paul@littlebluetech.com>
2011-03-17 19:21:56 -0400

    Remove one and attempt to fix other foreach_ macro.

--- mpx/pcb.h
+++ mpx/pcb.h
@@ -117,15 +117,7 @@ typedef struct {
 /* MACROS *
  * ------ */
 #define foreach_listitem( item, list ) \
-	for ( T * item = list->head; item != NULL; item = item->next )
-
-#define foreach_array_element( item, array ) \
-	for(int keep = 1, \
-		count = 0,\
-		size = sizeof (array) / sizeof *(array); \
-	keep && count != size; \
-	keep = !keep, count++) \
-	for(item = (array) + count; keep; keep = !keep)
+	for ( item = list->head; item != NULL; item = item->next )
 
 
 
