58c77b33da0507493147e9185b76b1069b78c172
Paul Prince <paul@littlebluetech.com>
2011-03-22 21:13:55 -0400

    Fix typo.

--- mpx/mpx_cmds.c
+++ mpx/mpx_cmds.c
@@ -379,7 +379,7 @@ void mpxcmd_ps ( int argc, char *argv[] )
 	printf("\nReversed:\n");
 	for ( i=3; i>=0; i-- ){
 
-		foreach_listenitem_rev( iter_node, queues[i], 1 ){
+		foreach_listitem_rev( iter_node, queues[i], 1 ){
 			printf("%16s %4d\n",
 				iter_node->pcb->name,
 				iter_node->pcb->priority
